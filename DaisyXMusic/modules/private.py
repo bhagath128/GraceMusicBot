1d3e6f17685da6b1b1f1dda8f1769635d56f8ab7
bhagath128 <85161649+bhagath128@users.noreply.github.com>
2021-06-09 17:00:28 +0530

    Create private.py

--- DaisyXMusic/modules/private.py
+++ DaisyXMusic/modules/private.py
@@ -36,7 +36,7 @@ async def start(_, message: Message):
                         "🧑🏻‍💻 Maintained By 🧑🏻‍💻", url=f"https://t.me/Surv_ivor")],
                 [
                     InlineKeyboardButton(
-                        "🛠 Source Code 🛠", url=f"https://{SOURCE_CODE}")
+                        "🛠 Source Code 🛠", url=f"https://t.me/marvelmoviesstuff")
                 ]
             ]
         )
@@ -50,7 +50,7 @@ async def gstart(_, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        text="Open Private", url=f"https://t.me/{BOT_USERNAME}?start"
+                        text="Open Private", url=f"https://t.me/Lisatelebot?start"
                     )
                 ]
             ]
@@ -90,8 +90,8 @@ def map(pos):
     elif(pos==len(tr.HELP_MSG)-1):
         url = f"https://t.me/{SUPPORT_GROUP}"
         button = [
-            [InlineKeyboardButton("➕ Add me to your Group 🙋‍♀️", url=f"https://t.me/{BOT_USERNAME}?startgroup=true")],
-            [InlineKeyboardButton(text = '🛠 Source Code 🛠', url=f"https://{SOURCE_CODE}")],
+            [InlineKeyboardButton("➕ Add me to your Group 🙋‍♀️", url=f"https://t.me/Lisatelebot?startgroup=true")],
+            [InlineKeyboardButton(text = '🛠 Source Code 🛠', url=f"https://t.me/marvelmoviesstuff")],
             [InlineKeyboardButton(text = '◀️', callback_data = f"help+{pos-1}")]
         ]
     else:
@@ -111,7 +111,7 @@ async def ghelp(_, message: Message):
             [
                 [
                     InlineKeyboardButton(
-                        "🟡 Click here for help 🟡", url=f"https://t.me/{BOT_USERNAME}?start"
+                        "🟡 Click here for help 🟡", url=f"https://t.me/Lisatelebot?start"
                     )
                 ]
             ]
